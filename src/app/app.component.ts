b7fed4a66b8c7ff8a343bf7e6bc495192df8759f
FabricioCruzz <fabriciocruzz77@gmail.com>
2022-03-24 21:55:40 -0300

    Subindo v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Time } from './time';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prova1-FabricioCruz';
+  
+  time: Time = {} as Time
+  
+  listaDeTimes: Time[] = []
+
+  saveData(form: NgForm) {
+    this.listaDeTimes.push(this.time)
+    this.time = {} as Time
+    this.ordenarArray()
+    form.resetForm()
+
+  }
+
+  ordenarArray() {
+    let swap: boolean;
+    do {
+      swap = false
+      for(let i = 0; i < this.listaDeTimes.length - 1; i++){
+        
+        if(this.listaDeTimes[i + 1] == null){
+          console.log("Passei pelo if do ordenaArray")
+          break;
+        }
+
+        else if(this.listaDeTimes[i].pontuacao < this.listaDeTimes[i + 1].pontuacao){
+          let timeAux: Time = {} as Time
+
+          timeAux = this.listaDeTimes[i]
+          this.listaDeTimes[i] = this.listaDeTimes[i + 1]
+          this.listaDeTimes[i + 1] = timeAux
+          
+          if(this.listaDeTimes[i].pontuacao < this.listaDeTimes[i + 1].pontuacao){
+            console.log('Helloo')
+          }
+
+          console.log("Estou no else if do ordena Array")
+          swap = true
+
+        }
+      }
+    } while (swap);
+  }
+
 }
